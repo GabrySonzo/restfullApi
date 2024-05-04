2b1123b7cbc8e332b5b9c9aab55009a3bc9534d1
TEP <you@example.com>
2024-05-04 08:45:36 +0000

    NON VAAAAA

--- server/index.php
+++ server/index.php
@@ -3,21 +3,21 @@ include "connessione.php";
 
 $metodo = $_SERVER['REQUEST_METHOD'];
 
-if($metodo == "GET"){
+if($metodo == 'GET'){
     $sql = "SELECT * FROM comuni";
     $result = $connessione->query($sql);
     $rows = array();
     while($row = $result->fetch_assoc()){
         $rows[] = $row;
     }
-    echo json_encode($rows);
-}else if($metodo == "POST"){
+    echo json_encode($metodo);
+}else if($metodo == 'POST'){
     $json = file_get_contents('php://input');
     $dati = json_decode($json);
-    $nome = $dati->nome;
-    $cap = $dati->cap;
-    $sql = "INSERT INTO comuni (nome, cap) VALUES ('$nome', '$cap')";
+    $nome = $dati["city"];
+    $cap = $dati["cap"];
+    $sql = "INSERT INTO comuni (nome, cap) VALUES ('pippo', '12345')";
     $connessione->query($sql);
-    echo "Inserimento avvenuto con successo";
+    echo json_encode($metodo);
 }
 ?>
